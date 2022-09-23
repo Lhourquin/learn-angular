30821e34fae52065340b3c2bef64c51e534abba1
lhourquin <lhourquin@gmail.com>
2022-09-23 23:39:43 +0200

    learn basic ngFor directives

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Book {
+  name: string,
+  author: string,
+  image: string
+}
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
@@ -7,16 +13,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
-  name :string = 'Clean code';
-  author:string = 'Robert Martin';
-  src:string = "https://images-na.ssl-images-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg";
+  books: Book[] = [
+    {
+      name: 'Clean code',
+      author: 'Robert Martin',
+      image: "https://images-na.ssl-images-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg",
 
-  name2 :string = "The Pragmatic Programmer";
-  author2 : string= "Andrew Hunt";
-  src2 : string = "https://images-eu.ssl-images-amazon.com/images/I/51W1sBPO7tL._SX218_BO1,204,203,200_QL40_ML2_.jpg";
+    },
+    {
+
+      name: "The Pragmatic Programmer",
+      author: "Andrew Hunt",
+      image: "https://images-eu.ssl-images-amazon.com/images/I/51W1sBPO7tL._SX218_BO1,204,203,200_QL40_ML2_.jpg"
+    }
+  ]
 
 
-  isShowing : boolean = true;
+
+
+  isShowing: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
